f06730c686d2ae10461c1f3a4c58579d99498702
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-10-04 17:33:45 +0300

    feat: general structure

---
+++ src/components/SearchBlock.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchBlock = () => {
+  return <div></div>;
+};
+
+export default SearchBlock;
---
+++ src/components/WeatherBlock.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WeatherBlock = () => {
+  return <div></div>;
+};
+
+export default WeatherBlock;
