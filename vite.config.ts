af88254fd619d2be99e6a6593bad71f6f3a45c4e
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-11-14 19:43:17 +0200

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { resolve } from 'path';
-import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,8 +30,5 @@ export default defineConfig({
       jsxRuntime: 'classic',
     }),
     tsconfigPaths(),
-    legacy({
-      targets: ['defaults', 'not IE11'],
-    }),
   ],
 });
