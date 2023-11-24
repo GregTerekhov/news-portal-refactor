bc81202ad05be27c94c641c84e4e0cd9dba7c29a
GregTerekhov <GregTerekhov@users.noreply.github.com>
2023-11-24 20:39:13 +0000

    deploy: 0d90dbb4bffb792981f1071f8b8244df5aedd2bc

---
+++ assets/ErrorPage-509e0a7f.js
@@ -0,0 +1,1 @@
+import{R as r}from"./index-60faf16b.js";const t=()=>r.createElement("div",null,"ErrorPage");export{t as default};
--- assets/ErrorPage-a6d26996.js
+++
@@ -1,1 +0,0 @@
-import{R as r}from"./index-de0e5d4d.js";const t=()=>r.createElement("div",null,"ErrorPage");export{t as default};
