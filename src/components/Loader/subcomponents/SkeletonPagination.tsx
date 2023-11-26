ff223fed6917b656b6b5c413b59d01e183b7719c
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-11-27 01:49:12 +0200

    optimise: SkeletonPagination, SkeletonWeather

--- src/components/Loader/subcomponents/SkeletonPagination.tsx
+++ src/components/Loader/subcomponents/SkeletonPagination.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
 
 const SkeletonPagination: FC = () => {
-  const paginationItemClass =
-    'w-10 h-10 bg-greyAlt/[.4] dark:bg-whiteBase/[.1] rounded-full transition-colors duration-500';
-  const paginationButtonClass =
-    'w-14 md:w-28 h-10 bg-greyAlt/[.4] dark:bg-whiteBase/[.1] rounded-[20px] transition-colors duration-500';
+  const commonClasses =
+    'h-10 bg-greyAlt/[.4] dark:bg-whiteBase/[.1] transition-colors duration-500';
+  const paginationItemClass = `${commonClasses} w-10 rounded-full`;
+  const paginationButtonClass = `${commonClasses} w-14 md:w-28 rounded-[20px]`;
 
   const paginationItems = [
     <div key={1} className={`${paginationButtonClass}`}></div>,
