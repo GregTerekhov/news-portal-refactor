99e1484f7bc580965d0dd235ec629e75cfbebc82
Max3281 <113448092+Max3281@users.noreply.github.com>
2024-02-04 15:31:51 +0300

    iconFix

--- src/layouts/Footer/assistants/renderSocialLinks.ts
+++ src/layouts/Footer/assistants/renderSocialLinks.ts
@@ -7,11 +7,6 @@ type SocialLinks = {
 export const renderSocialLinks = (): Array<SocialLinks> => {
   const links: SocialLinks[] = [
     {
-      link: 'https://www.youtube.com/@nytimes',
-      iconName: 'icon-arrow-right-filled',
-      label: 'YouTube',
-    },
-    {
       link: 'https://www.facebook.com/nytimes',
       iconName: 'icon-facebook',
       label: 'Facebook',
@@ -20,6 +15,11 @@ export const renderSocialLinks = (): Array<SocialLinks> => {
       link: 'https://twitter.com/nytimes',
       iconName: 'icon-twitter',
       label: 'Twitter',
+    },
+    {
+      link: 'https://www.youtube.com/@nytimes',
+      iconName: 'icon-youTube',
+      label: 'YouTube',
     },
   ];
 
