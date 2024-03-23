77352fd357b79749a3b6f2cc38c99ff45024fd28
Max3281 <113448092+Max3281@users.noreply.github.com>
2024-03-23 21:42:48 +0300

    fix:weatherState

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -52,5 +52,5 @@ export interface WeatherState {
   isLoading: boolean;
   data: WeatherData;
   weatherByHour: HourlyWeatherData | [];
-  hasError: string | number | null;
+  hasError: string | null;
 }
