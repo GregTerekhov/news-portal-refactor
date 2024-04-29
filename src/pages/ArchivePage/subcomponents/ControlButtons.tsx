f5e422f2869a8fc8d2efe491429ad56f76fa9543
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2024-04-29 22:17:13 +0300

    feat: ControlButtons decomposition

--- src/pages/ArchivePage/subcomponents/ControlButtons.tsx
+++ src/pages/ArchivePage/subcomponents/ControlButtons.tsx
@@ -4,46 +4,39 @@ import { useDBRedux } from 'reduxStore/hooks';
 import { useWindowWidthContext } from 'contexts';
 
 import { SvgIcon } from 'ui';
-
-interface ButtonsData {
-  type: 'submit' | 'reset' | 'button' | undefined;
-  iconName: string;
-  label: string;
-}
+import { getButtonsData } from '../assistants';
 
 const ControlButtons: FC<{ toggleModal: (e: React.MouseEvent<HTMLButtonElement>) => void }> = ({
   toggleModal,
 }) => {
-  const { getHistoryLog } = useDBRedux();
+  const { getHistoryLog, dbSuccessMessage } = useDBRedux();
   const { isNotMobile } = useWindowWidthContext();
 
-  const buttonStyles =
-    'group flex items-center gap-x-4 text-accentBase transition-colors duration-500 hocus:text-accentAlt dark:text-whiteBase dark:hocus:text-accentBase max-md:absolute max-md:top-0 max-md:h-10 max-md:w-10 max-md:justify-center';
+  const getButtonStyles = (iconName: string, disabled: boolean | undefined) => {
+    return `${disabled ? 'text-greyAlt dark:text-greyBase' : 'text-accentBase hocus:text-accentAlt dark:text-whiteBase dark:hocus:text-accentBase'} ${iconName === 'reset' ? 'max-md:right-12' : 'max-md:right-0'} group flex items-center gap-x-4 transition-colors duration-500 max-md:absolute max-md:top-0 max-md:h-10 max-md:w-10 max-md:justify-center`;
+  };
 
-  const iconStyles =
-    'fill-accentBase group-hover:fill-accentAlt group-focus:fill-accentAlt dark:fill-whiteBase dark:group-hover:fill-accentBase dark:group-focus:fill-accentBase';
+  const getIconStyles = (disabled: boolean | undefined) => {
+    return `${disabled ? 'fill-greyAlt dark:fill-greyBase' : 'fill-accentBase group-hover:fill-accentAlt group-focus:fill-accentAlt dark:fill-whiteBase dark:group-hover:fill-accentBase dark:group-focus:fill-accentBase'}`;
+  };
 
-  const buttonData: ButtonsData[] = [
-    { type: 'submit', iconName: 'reset', label: 'Update log' },
-    {
-      type: 'button',
-      iconName: 'trash',
-      label: 'Clear log',
-    },
-  ];
+  const isDisabledButton = dbSuccessMessage !== 'Remove news success';
+
+  const buttonData = getButtonsData(isDisabledButton);
 
   return (
     <>
       {Array.isArray(buttonData) &&
-        buttonData.map(({ type, iconName, label }) => (
+        buttonData.map(({ type, iconName, label, disabled }) => (
           <React.Fragment key={iconName}>
             <button
               type={type}
+              disabled={disabled}
               onClick={type === 'button' ? toggleModal : getHistoryLog}
-              className={`${buttonStyles} ${iconName === 'reset' ? 'max-md:right-12' : 'max-md:right-0'}`}
+              className={getButtonStyles(iconName, disabled)}
             >
               {isNotMobile ? label : null}
-              <SvgIcon svgName={iconName} sizeKey='smIcon18' className={iconStyles} />
+              <SvgIcon svgName={iconName} sizeKey='smIcon18' className={getIconStyles(disabled)} />
             </button>
           </React.Fragment>
         ))}
